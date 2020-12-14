c427315fdbe374efb4276b35c0d389a0d8677c2d
Charlie.Choe <charlie.choe@themedium.io>
2020-12-14 11:22:13 +0900

    article view 예외처리
    특정 서비스들은 pocket이 article view를 보여주지 못한다.

--- go.mod
+++ go.mod
@@ -1,18 +1,18 @@
 module github.com/whitekid/pocket-pick
 
-go 1.15
+go 1.16
 
 require (
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
 	github.com/allegro/bigcache v1.2.1
 	github.com/gorilla/sessions v1.2.1
 	github.com/labstack/echo-contrib v0.9.0
-	github.com/labstack/echo/v4 v4.1.17
+	github.com/labstack/echo/v4 v4.2.1
 	github.com/pkg/errors v0.9.1
-	github.com/spf13/cobra v1.1.1
+	github.com/spf13/cobra v1.1.3
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.7.0
 	github.com/swaggo/swag v1.7.0
-	github.com/whitekid/go-utils v0.0.0-20210203062500-f9d1f53542e3
+	github.com/whitekid/go-utils v0.0.0-20210210045943-8e9a4bf0b39e
 )
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
-	"github.com/whitekid/go-utils/log"
+	"github.com/whitekid/go-utils/flags"
 )
 
 const (
@@ -16,12 +16,7 @@ const (
 	keyCacheTimeout = "favorite_cache_timeout"
 )
 
-var configs = map[string][]struct {
-	key          string
-	short        string
-	defaultValue interface{}
-	description  string
-}{
+var configs = map[string][]flags.Flag{
 	"pocket-pick": {
 		{keyBind, "B", "127.0.0.1:8000", "bind address"},
 		{keyRootURL, "r", "http://127.0.0.0:8000", "root url"},
@@ -31,39 +26,18 @@ var configs = map[string][]struct {
 	},
 }
 
+func init() {
+	viper.SetEnvPrefix("pp")
+	viper.AutomaticEnv()
+
+	flags.InitDefaults(configs)
+}
+
+func InitFlagSet(use string, fs *pflag.FlagSet) { flags.InitFlagSet(configs, use, fs) }
+
+// Config access functions
 func BindAddr() string                    { return viper.GetString(keyBind) }
 func RootURL() string                     { return viper.GetString(keyRootURL) }
 func ConsumerKey() string                 { return viper.GetString(keyConsumerKey) }
 func AccessToken() string                 { return viper.GetString(keyAccessToken) }
 func CacheEvictionTimeout() time.Duration { return viper.GetDuration(keyCacheTimeout) }
-
-func init() {
-	// InitDefaults initialize config
-	for use := range configs {
-		for _, config := range configs[use] {
-			if config.defaultValue != nil {
-				viper.SetDefault(config.key, config.defaultValue)
-			}
-		}
-	}
-
-	viper.SetEnvPrefix("pp")
-	viper.AutomaticEnv()
-}
-
-// InitFlagSet cobra.Command와 연결
-func InitFlagSet(use string, fs *pflag.FlagSet) {
-	for _, config := range configs[use] {
-		switch v := config.defaultValue.(type) {
-		case string:
-			fs.StringP(config.key, config.short, v, config.description)
-		case time.Duration:
-			fs.DurationP(config.key, config.short, v, config.description)
-		case []byte:
-			fs.BytesHexP(config.key, config.short, v, config.description)
-		default:
-			log.Errorf("unsupported type %T", config.defaultValue)
-		}
-		viper.BindPFlag(config.key, fs.Lookup(config.key))
-	}
-}
